42d078d5533ebe0e25494682db79aee41c472de4
Markus Schräder <gitlab@priv.de>
2014-05-28 00:17:33 +0200

    remote sessions, timeout and error handling

--- tools/ldaptest.pl
+++ tools/ldaptest.pl
@@ -1,51 +1,74 @@
 #!/bin/perl
 
+use strict;
 use POE qw(Component::Client::LDAP);
+
+my $host = 'localhost';
+my $username = 'ldapbind';
+my $password = `cat password.txt`;
+chomp $password;        
+my $timeout = 10;
+my $session = undef;
+my $event = undef;
+my $base = "OU=TEST-OU,DC=adtest,DC=local";
+my $filter = '(&(objectClass=person)(memberof=CN=sayTRUST01,OU=TEST-OU,DC=adtest,DC=local))';
 
 POE::Session->create(
    inline_states => {
       _start => sub {
-         my ($heap, $session) = @_[HEAP, SESSION];
+         my ($heap, $session, $dstsession, $dstevent) = @_[HEAP, SESSION, ARG0, ARG1];
+         $heap->{dstsession} = $dstsession;
+         $heap->{dstevent} = $dstevent;
+         $poe_kernel->delay("timeout" => $timeout);
          $heap->{ldap} = POE::Component::Client::LDAP->new(
-            'localhost',
-            callback => $session->postback( 'connect' ),
+            $host,
+            callback => $session->postback('connect'),
          );
-         print "connecting\n";
       },
       connect => sub {
          my ($heap, $session, $callback_args) = @_[HEAP, SESSION, ARG1];
+         $poe_kernel->delay("timeout" => $timeout);
          if ( $callback_args->[0] ) {
-            print "connected\n";
             $heap->{ldap}->bind(
-               'ldapbind',
-               password => 'sayFUSE05',
-               callback => $session->postback( 'bind' ),
+               $username,
+               password => $password,
+               callback => $session->postback('bind'),
             );
          } else {
             delete $heap->{ldap};
-            print "Connection Failed\n";
+            $poe_kernel->delay("timeout" => undef);
+            $poe_kernel->post($heap->{dstsession} || $session => $heap->{dstevent} || "done" => undef => "connection failed");
          }
       },
       bind => sub {
          my ($heap, $session, $arg1, $arg2, $arg3, $arg4) = @_[HEAP, SESSION, ARG0, ARG1, ARG2, ARG3];
-         print "searching:".$heap->{ldap}.":".$arg1.":".$arg2.":".$arg3.":".$arg4.";\n";
+         $poe_kernel->delay("timeout" => $timeout);
          $heap->{ldap}->search(
-            base => "OU=TEST-OU,DC=adtest,DC=local",
-            filter => "(objectClass=person)",
-            callback => $session->postback( 'search' ),
+            base => $base,
+            filter => $filter,
+            callback => $session->postback('search'),
          );
       },
       search => sub {
-         my ($heap, $ldap_return) = @_[HEAP, ARG1];
+         my ($heap, $session, $ldap_return) = @_[HEAP, SESSION, ARG1];
          my $ldap_search = shift @$ldap_return;
-         print "results:".$ldap_search.":".$ldap_search->code.":\n";
-         $ldap_search->code && die $ldap_search->error;
-         foreach (@$ldap_return) {
-            print $_->dump;
-         }
-         delete $heap->{ldap} if $ldap_search->done;
+         $poe_kernel->post($heap->{dstsession} || $session => $heap->{dstevent} || "done" => [$ldap_search->entries] => ($ldap_search->code ? ($ldap_search->error || "unknown") : undef));
+         #if ($ldap_search->done) {
+            delete $heap->{ldap} ;
+            $poe_kernel->delay("timeout" => undef);
+         #}
+      },
+      timeout => sub {
+         my ($heap, $session) = @_[HEAP, SESSION];
+         delete $heap->{ldap};
+         $poe_kernel->post($heap->{dstsession} || $session => $heap->{dstevent} || "done" => undef => "timeout");
+      },
+      done => sub {
+         my ($heap, $data, $error) = @_[HEAP, ARG0, ARG1];
+         print "done: ".($data ? "".(join("\n\n----------\n\n", map { my $curattr = $_; join("\n", map { $_."=".join("#", @{$curattr->get_value ( $_, asref => 1 )}); } $curattr->attributes()) }  @$data)  )." " : "")."\n\n    ".($error ? "error: ".$error : "ok")."\n\n=========\n\n";
       },
    },
+   args => [$session => $event]
 );
 
 $poe_kernel->run();
