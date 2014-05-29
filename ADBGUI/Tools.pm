77ba96e68cadcbdc8fe13657a61fb51b380ea92c
Markus Schraeder <gitlab@priv.de>
2014-05-29 22:56:35 +0200

    queryLDAP: tools/ldaptest.pl -> ADBGUI/Tools.pm

--- ADBGUI/Tools.pm
+++ ADBGUI/Tools.pm
@@ -40,6 +40,76 @@ sub removeTime {
    return $text;
 }
 
+sub queryLDAP {
+   my $config = shift;
+   return undef
+      unless (
+         $config->{host} &&
+         $config->{callback} &&
+         $config->{ldapbase} && 
+ defined($config->{ldapfilter}) &&
+         $config->{username} &&
+         $config->{password});
+   POE::Session->create(
+      inline_states => {
+         _start => sub {
+            my ($heap, $session, $config) = @_[HEAP, SESSION, ARG0, ARG1, ARG2];
+            $heap->{config} = $config;
+            $heap->{config}->{timeout} ||= 10;
+            $poe_kernel->delay("timeout" => $heap->{config}->{timeout});
+            $heap->{ldap} = POE::Component::Client::LDAP->new(
+               $heap->{config}->{host},
+               callback => $session->postback('connect'),
+            );
+         },
+         connect => sub {
+            my ($heap, $session, $callback_args) = @_[HEAP, SESSION, ARG1];
+            $poe_kernel->delay("timeout" => $heap->{config}->{timeout});
+            if ( $callback_args->[0] ) {
+               $heap->{ldap}->bind(
+                  $heap->{config}->{username},
+                  password => $heap->{config}->{password},
+                  callback => $session->postback('bind'),
+               );
+            } else {
+               delete $heap->{ldap};
+               $poe_kernel->delay("timeout" => undef);
+               $poe_kernel->post($heap->{config}->{dstsession} || $session => $heap->{config}->{dstevent} || "done" => undef => "connection failed");
+            }
+         },
+         bind => sub {
+            my ($heap, $session, $arg1, $arg2, $arg3, $arg4) = @_[HEAP, SESSION, ARG0, ARG1, ARG2, ARG3];
+            $poe_kernel->delay("timeout" => $heap->{config}->{timeout});
+            $heap->{ldap}->search(
+               base => $heap->{config}->{ldapbase},
+               filter => $heap->{config}->{ldapfilter},
+               callback => $session->postback('search'),
+            );
+         },
+         search => sub {
+            my ($heap, $session, $ldap_return) = @_[HEAP, SESSION, ARG1];
+            my $ldap_search = shift @$ldap_return;
+            $poe_kernel->post($heap->{config}->{dstsession} || $session => $heap->{config}->{dstevent} || "done" => [$ldap_search->entries] => ($ldap_search->code ? ($ldap_search->error || "unknown") : undef));
+            #if ($ldap_search->done) {
+               delete $heap->{ldap} ;
+               $poe_kernel->delay("timeout" => undef);
+            #}
+         },
+         timeout => sub {
+            my ($heap, $session) = @_[HEAP, SESSION];
+            delete $heap->{ldap};
+            $poe_kernel->post($heap->{config}->{dstsession} || $session => $heap->{config}->{dstevent} || "done" => undef => "timeout");
+         },
+         done => sub {
+            my ($heap, $data, $error) = @_[HEAP, ARG0, ARG1];
+            #print "done: ".($data ? "".(join("\n\n----------\n\n", map { my $curattr = $_; join("\n", map { $_."=".join("#", @{$curattr->get_value ( $_, asref => 1 )}); } $curattr->attributes()) }  @$data)  )." " : "")."\n\n    ".($error ? "error: ".$error : "ok")."\n\n=========\n\n";
+            $config->{callback}($heap->{config}, $data, $error, $heap);
+         },
+      },
+      args => [$config],
+   );
+}
+
 sub ReadConfig {
    my $configfile = shift;
    my $keys = shift || [];
