d8127dcc143f42888357d010504a6f2ac736379e
Markus Schraeder <gitlab@priv.de>
2014-08-09 12:50:46 +0200

    More showdiff improvements

--- install/show.diff.sh
+++ install/show.diff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export GIT_PAGER=cat
-git diff; for i in `ls|sort`; do [[ -d $i && "ls $i/*.pm" != "" ]] && echo \[$i\] && echo && cd $i 2>/dev/null && git diff -w && cd ..; done
+git diff; for i in `ls|sort`; do [[ "$i" != "Documentation" ]] && [[ "$i" != "ADBGUI" ]]  && [[ "$i" != "install" ]] && [ -d $i ] && [[ `ls $i/*.pm 2>/dev/null` != "" ]] && echo \[$i\] && echo && cd $i 2>/dev/null && git diff -w && cd ..; done
 
