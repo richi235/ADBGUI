e78f1bd0e7a38972f33b7c1d1f228607683c39a0
Markus Schraeder <gitlab@priv.de>
2014-08-09 13:04:06 +0200

    Filter module

--- install/show.diff.sh
+++ install/show.diff.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 export GIT_PAGER=cat
-git diff; for i in `ls|sort`; do [[ "$i" != "Documentation" ]] && [[ "$i" != "ADBGUI" ]]  && [[ "$i" != "install" ]] && [ -d $i ] && [[ `ls $i/*.pm 2>/dev/null` != "" ]] && echo && echo \[$i\] && echo && cd $i 2>/dev/null && git diff -w && cd ..; done
+export list=`ls|sort`;
+if [[ "$1" != "" ]]; then
+   export list=`ls|grep $1|sort`;
+   if [[ "$1" == "ADBGUI" ]]; then
+      echo;
+      echo "[ADBGUI]";
+      echo;
+      git diff;
+      exit 0;
+   fi;
+else
+   echo;
+   echo "[ADBGUI]";
+   echo;
+   git diff;
+fi;
+
+for i in $list; do [[ "$i" != "Documentation" ]] && [[ "$i" != "ADBGUI" ]] && [[ "$i" != "install" ]] && [ -d $i ] && [[ `ls $i/*.pm 2>/dev/null` != "" ]] && echo && echo \[$i\] && echo && cd $i 2>/dev/null && git diff -w && cd ..; done
 
