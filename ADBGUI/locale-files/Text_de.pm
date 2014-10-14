41c83521642e0839d9c36dbd7157ccf380fd8bea
Richard Sailer <richard@weltraumpflege.org>
2014-10-14 06:53:53 +0200

    Set encoding of Text_*.pm files to utf-8

--- ADBGUI/locale-files/Text_de.pm
+++ ADBGUI/locale-files/Text_de.pm
@@ -84,7 +84,7 @@ sub new
       B_ADD             => 'Eintrag hinzuf&uuml;gen',
       B_ANALYSE         => 'zu den Log-Dateien',
       B_CONFIG          => 'Filter konfigurieren',
-      B_DEL             => 'Löschen',
+      B_DEL             => 'LÃ¶schen',
       B_UNDEL           => 'Wiederherstellen',
       B_OVERVIEW        => 'Uebersicht',
       B_EDIT            => 'Editieren',
@@ -95,11 +95,11 @@ sub new
       B_USE_FILTER      => 'Filter anwenden',
       B_LOGOUT          => 'Ausloggen',
       B_ACTION          => 'Aktionen',
-      B_BOSS            => 'BOSS Button - Aenderungen JETZT übernehmen',
-      B_TERM            => 'Tunnel schließen',
-      B_OPEN            => 'Tunnel öffnen',
+      B_BOSS            => 'BOSS Button - Aenderungen JETZT Ã¼bernehmen',
+      B_TERM            => 'Tunnel schlieÃŸen',
+      B_OPEN            => 'Tunnel Ã¶ffnen',
       B_SHOW_ALL        => 'Alles anzeigen',
-      B_SHOW_OPENED     => 'Tunnel öffnen',
+      B_SHOW_OPENED     => 'Tunnel Ã¶ffnen',
       B_SCHEDULE        => 'Plan',
       B_CHANGEPASS      => 'Passwort aendern',
       B_CRYPTOCARD      => 'Cryptocard initialisieren',
@@ -108,8 +108,8 @@ sub new
 
       T_UNDEL      => 'Ja, wirklich wiederherstllen.',
       T_DEL        => 'Ja, wirklich loeschen.',
-      T_DELNO      => 'Nein, zurück zur &Uuml;bersicht',
-      T_UNDELNO    => 'Nein, zurück zur &Uuml;bersicht',
+      T_DELNO      => 'Nein, zurÃ¼ck zur &Uuml;bersicht',
+      T_UNDELNO    => 'Nein, zurÃ¼ck zur &Uuml;bersicht',
 
       WHITELIST    => 'ALLE',
       
@@ -151,12 +151,12 @@ sub new
           context_error           =>  "(Fehler beim Erstellen des Kontexts: ",
           internal_error          =>  "Interner Fehler",
           permission_denied       =>  "Fehlende Zugriffsberechtigung",
-          popupid_missing         =>  "Die ID für das Popup-Window für folgende Aktion fehlt: ",
+          popupid_missing         =>  "Die ID fÃ¼r das Popup-Window fÃ¼r folgende Aktion fehlt: ",
           qx_context_error        =>  "Interner Fehler beim Erstellen des folgenden Qooxdoo Contexts:",
           col_load_error          =>  "Fehler beim Laden folgender Tabellen-Spalte: '",
           unknown_command         =>  "Unbekannte Aktion: ",
           table_non_modifiable    =>  "Tabelle nicht bearbeitbar",
-          delrow_failed           =>  "Das Löschen der Zeile ist fehlgeschlagen.",
+          delrow_failed           =>  "Das LÃ¶schen der Zeile ist fehlgeschlagen.",
           
           paths     =>
           {
@@ -164,7 +164,7 @@ sub new
               system_search_png  => "resource/qx/icon/Tango/16/actions/system-search.png",
               list_add           => "resource/qx/icon/Tango/16/actions/list-add.png",
           },
-          # potentielle übersetzungskandidaten:
+          # potentielle Ã¼bersetzungskandidaten:
 
           stats_window =>
           {
