aaa415110116d91122f175684c4e517707faf88f
Richard Sailer <richard@weltraumpflege.org>
2014-10-14 06:43:33 +0200

    added set-language scripts

---
+++ ADBGUI/enable_german_localisation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+### The official ADBGUI localisation switcher (TM)^^ ###
+### Just execute to switch to german localisation
+
+    # overwrite the current lang file with the german ones
+    cp ./locale-files/Text_de.pm Text.pm
