2bfe315f10572896bc41c86b4ecab56ecbc59c02
Markus Schräder <gitlab@priv.de>
2015-01-11 18:04:10 +0100

    Allow to specify cmd on show.diff.sh

--- install/show.diff.sh
+++ install/show.diff.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 export GIT_PAGER=cat
 export list=`ls|sort`;
+export cmd='(git diff --color -w); (git status)'
+if [[ "$CMD" != "" ]]; then
+   export cmd="$CMD"
+fi;
 if [[ "$1" != "" ]]; then
    export list=`ls|grep $1|sort`;
    if [[ "$1" == "ADBGUI" ]]; then
       echo;
       echo "[ADBGUI]";
       echo;
-      git diff;
-      git status;
+      bach -c "$cmd";
       exit 0;
    fi;
 else
    echo;
    echo "[ADBGUI]";
    echo;
-   git diff;
-   git status;
+   bash -c "$cmd";
 fi;
 
-for i in $list; do [[ "$i" != "Documentation" ]] && [[ "$i" != "ADBGUI" ]] && [[ "$i" != "install" ]] && [ -d $i ] && [[ `ls $i/*.pm 2>/dev/null` != "" ]] && echo && echo \[$i\] && echo && cd $i 2>/dev/null && git diff -w && git status && cd ..; done
+for i in $list; do [[ "$i" != "Documentation" ]] && [[ "$i" != "ADBGUI" ]] && [[ "$i" != "install" ]] && [ -d $i ] && [[ `ls $i/*.pm 2>/dev/null` != "" ]] && echo && echo \[$i\] && echo && cd $i 2>/dev/null && bash -c "$cmd" && cd ..; done
 
